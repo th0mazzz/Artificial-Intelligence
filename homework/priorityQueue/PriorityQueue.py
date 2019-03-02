ed47d32ad1ffd9912cf5e1889d0a431672ff556b
th0mazzz <thomaszhao2001@gmail.com>
2019-03-01 23:40:38 -0500

    Versatilized via compare_to

--- homework/priorityQueue/PriorityQueue.py
+++ homework/priorityQueue/PriorityQueue.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 
 class Pqueue:
-
-    def __init__(self, optional = None): #initializes priority queue / heap
+    def OrdinaryComparison(a,b): #ordinary compare_to fxn
+        if a < b: return -1
+        if a == b: return 0
+        return 1
+    
+    def __init__(self, comparator = OrdinaryComparison): #initializes priority queue / heap
         self.info = []
         self.size = 0
-
-        if not optional == None:
-            for each in optional:
-                self.push(each)
+        self.compare_to = comparator
                 
     def __str__(self): #the to-string method
         return str(self.info)
+
     
     def push(self, value): #pushs value into heap; sorts it so that parent >= child
         self.info.append(value)
@@ -20,7 +22,7 @@ class Pqueue:
         current = self.size - 1
         parent = int((current - 1) / 2)
 
-        while(self.info[current] < self.info[parent] and parent >= 0):
+        while(self.compare_to(self.info[current], self.info[parent]) == -1 and parent >= 0):
             self.info[current], self.info[parent] = (self.info[parent], self.info[current])
 
             current = parent
@@ -43,14 +45,14 @@ class Pqueue:
         #print('leftchild: ' + str(leftchild))
         #print('rightchild: ' + str(rightchild))
 
-        if self.size == 2 and self.info[current] > self.info[leftchild]:
+        if self.size == 2 and self.compare_to(self.info[current], self.info[leftchild]) == 1:
             self.info[current], self.info[leftchild] = (self.info[leftchild], self.info[current])
         
         while(leftchild < self.size and rightchild < self.size and
-              (self.info[current] > self.info[leftchild] or self.info[current] > self.info[rightchild])):
+              (self.compare_to(self.info[current], self.info[leftchild]) == 1 or self.compare_to(self.info[current], self.info[rightchild]) == 1)):
             print('leftchild: ' + str(leftchild))
             print('rightchild: ' + str(rightchild))
-            if self.info[leftchild] < self.info[rightchild]:
+            if self.compare_to(self.info[leftchild], self.info[rightchild]) == -1:
                 self.info[current], self.info[leftchild] = (self.info[leftchild], self.info[current])
                 current = leftchild
                 leftchild = current * 2 + 1
@@ -85,6 +87,7 @@ a.push(8)
 a.push(12)
 a.push(100)
 a.push(22)
+a.push(2)
 
 #a.pop()
 #a.pop()
