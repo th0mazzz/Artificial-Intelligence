1e3db00a3d1d8411fbec2512f2ee7006af1f48c9
th0mazzz <thomaszhao2001@gmail.com>
2019-04-02 21:39:41 -0400

    got some results

--- wordneighborexploration/Exploration.py
+++ wordneighborexploration/Exploration.py
@@ -10,21 +10,33 @@ wordset = list(wordlistfile.read().split('\n'))
 wordlistfile.close()
 
 
+
+
 def dropone(inputfilename):
     #Opens files that need to be read
-    inputfile = open(inputfilename, 'rU')
-    inputwords = inputfile.read().split('\n')
-    inputfile.close()
+    #inputfile = open(inputfilename, 'rU')
+    #inputwords = inputfile.read().split('\n')
+    #inputfile.close()
     
-    if inputwords[len(inputwords) - 1] == '':
-        inputwords.remove('')
+    #if inputwords[len(inputwords) - 1] == '':
+    #    inputwords.removes('')
+
+    inputwords = []
+    
+    for each in wordset:
+        if len(each) == 5:
+            inputwords.append(each)
 
     #print(inputwords)
     #print(wordset)
 
     le_permutations = []
+
+    winner_word = None
+    winner_anagrams = []
     
     for word in inputwords:
+        print(word)
         index = len(word)
         while index > 0:
             perms = permutations(list(word),index)
@@ -35,23 +47,30 @@ def dropone(inputfilename):
             
         #print(le_permutations)
         for tupple in le_permutations:
-            word = ''
+            yum = ''
             for letter in tupple:
-                word = word + letter
+                yum = yum + letter
 
-            le_permutations_words.append(word)
+            le_permutations_words.append(yum)
 
         #print(le_permutations_words)
 
-        final = []
+        final = set()
         
         for permmy in le_permutations_words:
             if permmy in wordset:
-                final.append(permmy)
+                final.add(permmy)
 
         le_permutations = []
-        print(final)
+        #print(final)
+        #print('\n')
+
+        if len(winner_anagrams) <= len(final):
+            winner_word = word
+            winner_anagrams = final
+
+    return (winner_word, winner_anagrams, len(winner_anagrams))
 
     
 
-dropone(sys.argv[1])
+print(dropone(sys.argv[1]))
