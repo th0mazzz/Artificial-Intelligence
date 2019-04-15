6f9256e0f548cca477ea2ad2745f7393bb70e3b0
th0mazzz <thomaszhao2001@gmail.com>
2019-04-14 21:45:45 -0400

    mostly working, too much stuff happening after correct answr tho:

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -1,11 +1,8 @@
 #!/bin/usr/python3
 
 #-------- GLOBAL VARS --------
-current_state = 'NEW_CELL'
-digits = set([1,2,3,4,5,6,7,8,9])
-stack = []
-nback = 0
-ntrials = 0
+numBack = 0
+numTrials = 0
 board = []
 cliques = [[0,1,2,3,4,5,6,7,8],
          [9,10,11,12,13,14,15,16,17],
@@ -37,24 +34,93 @@ cliques = [[0,1,2,3,4,5,6,7,8],
 
 import sys
 
-def nextOpenCell(the_board, current_cell):
-    nextCell = current_cell + 1
-    if nextCell >= len(the_board):
-        return None
-    elif the_board[nextCell] == '_':
-        return nextCell
+def createBoard(inputfilename, puzzlename):
+    with open(inputfilename) as file:
+        content = file.readlines()
+        content = [x.strip() for x in content]
+    #print(content)
+
+    puzzle = []
+
+    if puzzlename == 'A1-1,Easy-NYTimes,unsolved':
+        puzzlenameIndex = content.index('A1-1,Easy-NYTimes,unsolved')
+    elif puzzlename == 'A2-1,Medium-NYTimes,unsolved':
+        puzzlenameIndex = content.index('A2-1,Medium-NYTimes,unsolved')
     else:
-        return nextOpenCell(the_board, nextCell)
+        puzzlenameIndex = content.index('A3-1,Hard-NYTimes,unsolved')
+
+    puzzleTemp = []
+    for i in range(9):
+        puzzleTemp.append(content[i + 1 + puzzlenameIndex])
+    for line in puzzleTemp:
+        split = line.split(',')
+        board.extend(split)
 
 
-def main():
-    file = open('board.txt', 'rU')
-    lines = file.read().split('\n')
-    for line in lines:
-        tile = line.split(',')
-        board.extend(tile)
-    print(board)
+def printBoard(the_board):
+    print(the_board[0:9])
+    print(the_board[9:18])
+    print(the_board[18:27])
+    print(the_board[27:36])
+    print(the_board[36:45])
+    print(the_board[45:54])
+    print(the_board[54:63])
+    print(the_board[63:72])
+    print(the_board[72:])
 
-    
+    #for i in range(8)
 
-main()
+def getNextTile(the_board, the_current):
+    if the_current == len(board) - 1:
+        #print('getNextTile(): None')
+        return None
+    else:
+        while not the_board[the_current] == '_':
+            the_current = the_current + 1
+        #print('getNextTile(): ' + str(the_current))
+        return the_current
+
+def isValid(the_board, the_current, the_guess):
+    importante = set()
+    for cliq in cliques:
+        if the_current in cliq:
+            for each in cliq:
+                if not each == the_current:
+                    importante.add(each)
+    #print(the_board)
+    for each in importante:
+        if the_board[each] == str(the_guess):
+            return False
+    return True
+
+def main(inputBoards, outputname, puzzlename):
+    createBoard(inputBoards, puzzlename)
+    mainHelper(board, 0)
+
+i = 0
+
+def mainHelper(the_board, current_tile):
+    global i
+    i = i + 1
+    print(i)
+    global numTrials
+    global numBack
+    if current_tile == None:
+        printBoard(the_board)
+        print('numTrials: ' + str(numTrials))
+        print('numBacks: ' + str(numBack))
+        return the_board
+
+    digits = [1,2,3,4,5,6,7,8,9]
+    for guess in digits:
+        numTrials = numTrials + 1
+        if isValid(the_board, current_tile, guess):
+            the_board[current_tile] = str(guess)
+            mainHelper(the_board[:], getNextTile(the_board[:], current_tile))
+
+    numBack = numBack + 1
+
+
+
+
+main(sys.argv[1], sys.argv[2], sys.argv[3]) #$python <this file> <input boards> <output name> <which puzzle>
